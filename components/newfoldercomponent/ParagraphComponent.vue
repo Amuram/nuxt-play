a6a9200446ef0cb21d777d47db0c0a58536fac12
Aaron <onmokeaaronjude@gmail.com>
2022-09-06 16:15:01 +0100

    created a PageComponent and three other components..

---
+++ components/newfoldercomponent/ParagraphComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div  contenteditable="true"></div>
+  </div>
+</template>
+
+<script>
+export default {
+
+    props: [
+        'paragraphs'
+    ]
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/PageComponent.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+    <ParagraphComponent :paragraphs="paragraphs" />
+  </div>
+</template>
+
+<script>
+import ParagraphComponent from '~/components/newfoldercomponent/ParagraphComponent.vue';
+export default {
+    data(){
+    return {
+          
+            'normal_text': {
+              'bold': true,
+              'italic': true,
+              'underline': true,
+
+            },
+            'main_heading': {
+              'bold': false,
+              'italic': true,
+              'underline': true,
+
+            },
+            'sub_heading': {
+              'bold': false,
+              'italic': true,
+              'underline': true,
+
+            },
+            'quote': {
+              'bold': false,
+              'italic': true,
+              'underline': true,
+
+            },
+
+            'bold': 'flok-nl-bold',
+            'underline': 'flok-nl-underline',
+            'italics': 'flok-nl-italics',
+            'quote': 'flok-nl-quote',
+            'main_heading': 'flok-nl-main-heading',
+            'sub_heading': 'flok-nl-sub-heading',
+
+         paragraphs: [   
+                    {
+                        'type': 'normal-text',
+                        'content': 'Hello world',
+                        'classes': ['flok-nl-bold', 'flok-nl-underline'],
+                        'generated_html': '<div class="flok-nl-bold flok-nl-underline">Hello World</div>'
+                    },
+                    {
+                        'type': 'normal-text',
+                        'content': 'Hello world',
+                        'classes': ['flok-nl-italic', 'flok-nl-underline'],
+                        'generated_html': '<div class="flok-nl-italic flok-nl-underline">Hello World</div>'
+                    },
+                    {
+                        'type': 'quote',
+                        'content': 'Hello world',
+                        'classes': ['flok-nl-quote'],
+                        'generated_html': '<div class="flok-nl-quote">Hello World</div>'
+                    },
+]
+
+          
+    }
+  },
+    components: { ParagraphComponent }
+}
+</script>
+
+<style>
+
+</style>
+
